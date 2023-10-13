c71bff5bb4423a818c04a1a78943784abe324d9a
Thayrov Garcia <105318081+Thayrov@users.noreply.github.com>
2023-10-13 16:10:31 -0600

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -1,10 +1,4 @@
-# En esta aula te hablaremos sobre la historia de SQL y de My SQL.
-# También instalaremos Workbench para poder utilizar un IDE en el desarrollo de nuestros ejercicios.
-
-# Ya tu ambiente debe estar listo para trabajar. Te invito ver los videos y a ejecutar todos los 
-# comandos que vas a aprender.
-
-#Los comandos vistos en el aula anterior son los siguientes:
+#Los comandos vistos en las clases son los siguientes:
 
 USE world;
 
